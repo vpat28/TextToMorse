da77e04dd659a439997524556b367794d354e4e6
Vraj Patel <jmffhrf@jmfamily.com>
2024-09-06 12:34:22 -0400

    init commit

---
+++ MorseFunct.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using TexttoMorse;
+
+public class MorseFunct
+{
+    public static Dictionary<char, string> morseCodeDictionary = new Dictionary<char, string>
+        {
+            { 'A', ".-" }, { 'B', "-..." }, { 'C', "-.-." }, { 'D', "-.." },
+            { 'E', "." }, { 'F', "..-." }, { 'G', "--." }, { 'H', "...." },
+            { 'I', ".." }, { 'J', ".---" }, { 'K', "-.-" }, { 'L', ".-.." },
+            { 'M', "--" }, { 'N', "-." }, { 'O', "---" }, { 'P', ".--." },
+            { 'Q', "--.-" }, { 'R', ".-." }, { 'S', "..." }, { 'T', "-" },
+            { 'U', "..-" }, { 'V', "...-" }, { 'W', ".--" }, { 'X', "-..-" },
+            { 'Y', "-.--" }, { 'Z', "--.." },
+            { '0', "-----" }, { '1', ".----" }, { '2', "..---" }, { '3', "...--" },
+            { '4', "....-" }, { '5', "....." }, { '6', "-...." }, { '7', "--..." },
+            { '8', "---.." }, { '9', "----." },
+            { '.', ".-.-.-" }, { ',', "--..--" }, { '?', "..--.." }, { '!', "-.-.--" },
+            { ':', "---..." }, { ';', "-.-.-." }, { '(', "-.--." }, { ')', "-.--.-" },
+            { '&', ".-..." }, { '=', "-...-" }, { '+', ".-.-." }, { '-', "-....-" },
+            { '_', "..--.-" }, { '"', ".-..-." }, { '$', "...-..-" }, { '@', ".--.-." },
+            { '\'', ".----." }, { '/', "-..-." }, {' '," " }
+        };
+    static void ConvertToMorse(string input)
+    {
+        string MorseEquivalent;
+        foreach (char c in input)
+        {
+            MorseEquivalent = morseCodeDictionary.GetValueOrDefault(c, null);
+            Console.Write(MorseEquivalent);
+            Console.Write("  ");
+            if (c.Equals(' '))
+            {
+                Console.WriteLine("\n");
+            }
+        }
+    }
+
+    public static void TranslateToMorse(string input)
+    {
+        //universal string end character
+        char delimiter = '\u001F';
+        input = input + delimiter;
+        string MorseEquivalent;
+        string tempword = " ";
+        CharEnumerator parse = input.GetEnumerator();
+        bool test = false;
+        while (!test)
+        {
+            parse.MoveNext();
+            char c = parse.Current;
+            // Console.WriteLine("!" + c.ToString() + "-");
+
+            try
+            {
+                MorseEquivalent = morseCodeDictionary.GetValueOrDefault(c, null);
+                tempword = tempword + c;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(MorseEquivalent);
+                if (!c.ToString().Equals(" ") && !(c.Equals(delimiter)))
+                {
+                    string whitespaceString = new string(' ', MorseEquivalent.Length);
+                    if (MorseEquivalent.Length == 1)
+                    {
+                        tempword += whitespaceString + " ";
+                    }
+                    else
+                    {
+                        tempword += whitespaceString + " ";
+                    }
+
+                }
+                Console.Write("  ");
+                if (c.Equals(' ') || c.Equals(delimiter))
+                {
+                    Console.WriteLine("\n");
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(tempword);
+                    Console.WriteLine("\n");
+                    tempword = "";
+                    Console.WriteLine();
+                    if (c.Equals(delimiter))
+                    {
+                        test = true;
+                        break;
+                    }
+                }
+            }
+
+            catch (Exception e) {
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.Write("\n" + "The character: ");
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(c.ToString()); 
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(" does NOT exist in Morse Code. Try something else.");
+                Console.WriteLine("");
+
+                Program.EndofGame();
+                throw new Exception("Not a valid morse code character!"); 
+                
+            }
+
+
+        }
+        
+    }
+
+  
+    public static void fileToMorse(string filename)
+    {
+
+        try
+        {
+            // Open the text file using a stream reader.
+            using StreamReader reader = new StreamReader(filename);
+
+            // Read the stream as a string.
+            string text = reader.ReadToEnd();
+            // string text = reader.ReadLine();
+
+            //foreach (char c in text)
+            //{
+            //    TranslateToMorse(c.ToString());
+            //}
+            //calling convert method
+
+
+
+            // Write the text to the console.
+             Console.WriteLine(text);
+            text  = text.ToUpper();
+            text = text.TrimEnd();
+            text = text.TrimStart();
+            TranslateToMorse(text);
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿namespace TexttoMorse
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Thread.Sleep(1000);  Console.WriteLine("████████╗███████╗██╗  ██╗████████╗               ████████╗ ██████╗                ███╗   ███╗ ██████╗ ██████╗ ███████╗███████╗      \r\n╚══██╔══╝██╔════╝╚██╗██╔╝╚══██╔══╝               ╚══██╔══╝██╔═══██╗               ████╗ ████║██╔═══██╗██╔══██╗██╔════╝██╔════╝      \r\n   ██║   █████╗   ╚███╔╝    ██║   █████╗   █████╗   ██║   ██║   ██║█████╗   █████╗██╔████╔██║██║   ██║██████╔╝███████╗█████╗        \r\n   ██║   ██╔══╝   ██╔██╗    ██║   ╚════╝   ╚════╝   ██║   ██║   ██║╚════╝   ╚════╝██║╚██╔╝██║██║   ██║██╔══██╗╚════██║██╔══╝        \r\n   ██║   ███████╗██╔╝ ██╗   ██║         ██╗         ██║   ╚██████╔╝      ██╗      ██║ ╚═╝ ██║╚██████╔╝██║  ██║███████║███████╗██╗██╗\r\n   ╚═╝   ╚══════╝╚═╝  ╚═╝   ╚═╝         ╚═╝         ╚═╝    ╚═════╝       ╚═╝      ╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝╚═╝\r\n                                                                                                                                    ");
+            GetUserInput();
+        }
+        public static void EndofGame()
+        {
+            bool isValid = false;
+            while (!isValid)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Press 'R' to restart. Press 'Q' to quit.");
+                string userinput = Console.ReadLine();
+
+                switch (userinput.ToLower())
+                {
+                    case "r":
+                        Console.Clear();
+                        GetUserInput();
+                        isValid = true;
+                        break;
+                    case "q":
+                        System.Environment.Exit(0);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        isValid = true;
+                        break;
+                    default:
+                        Console.WriteLine("Please pick a valid option!");
+                        Thread.Sleep(1500); Console.Clear();
+                        break;
+
+                }
+            }
+        }
+
+        static void GetUserInput()
+        {
+            bool validinput = false;
+            while (!validinput)
+            {
+                Console.WriteLine("Press 'F' to enter a file path \n Press 'T' to enter your own text to be converted ");
+                string userInput = Console.ReadLine();
+                switch (userInput.ToLower())
+                {
+                    case "f":
+                        Console.WriteLine("Enter a file path: ");
+                        string fileInput = Console.ReadLine();
+                        MorseFunct.fileToMorse(fileInput);
+                        validinput = true;
+                        break;
+                    case "t":
+                        Console.WriteLine("Enter your text: ");
+                        string textInput = Console.ReadLine().ToUpper();
+                        MorseFunct.TranslateToMorse(textInput);
+                        validinput = true;
+                        break;
+                    default:
+                        
+                        Console.WriteLine("Please pick a valid option!");
+                        Thread.Sleep(1500); Console.Clear();
+                        break;
+                }
+
+                EndofGame();
+            }
+        }
+
+
+
+    }
+}
